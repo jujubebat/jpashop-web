0af4215565ad6faa924016a5be166aa42fb1e673
Byung Wook Park <bbwwpark@naver.com>
2021-09-25 20:14:02 +0900

    feat: 회원 엔티티 구현

--- src/test/java/jpabook/jpashop/MemberRepositoryTest.java
+++ src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -2,6 +2,7 @@ package jpabook.jpashop;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import jpabook.jpashop.domain.Member;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
